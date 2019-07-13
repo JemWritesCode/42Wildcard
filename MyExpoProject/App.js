e9268dac6113cb10df2bd4443aec6ad7015902ad
Jem Cope <thejemmeh@gmail.com>
2019-07-12 20:24:24 -0700

    ReactNativeTest

--- MyExpoProject/App.js
+++ MyExpoProject/App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Change up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+  state = {
+    placeName: ""
+  };
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+    return (
+      <View style={styles.container}>
+        <Text>Hi.</Text>
+        <TextInput
+        style={{width: 300, borderColor: "black", borderWidth: 1}}
+        value={this.state.placeName} 
+        onChangeText={this.placeNameChangedHandler}
+        />
+      </View>
+    );
+  };
+  
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', 
   },
 });
